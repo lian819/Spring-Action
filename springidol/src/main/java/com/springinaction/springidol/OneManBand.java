4fc8325930c511ed60e78aef7a3fb19bdf1555f7
lian819 <lianxinzhong@126.com>
2015-10-10 01:51:31 +0800

    practice wiring bean

--- springidol/src/main/java/com/springinaction/springidol/OneManBand.java
+++ springidol/src/main/java/com/springinaction/springidol/OneManBand.java
@@ -1,22 +1,26 @@
-//<start id="onemanband_java" /> 
 package com.springinaction.springidol;
 
 import java.util.Collection;
 
+/**
+ * @author Lian
+ * @time 2015年10月10日 上午1:41:27
+ * @desc 一个乐队的参赛者
+ */
 public class OneManBand implements Performer {
-  public OneManBand() {
-  }
+	public OneManBand() {
+	}
 
-  public void perform() throws PerformanceException {
-    for (Instrument instrument : instruments) {
-      instrument.play();
-    }
-  }
+	public void perform() throws PerformanceException {
+		for (Instrument instrument : instruments) {
+			instrument.play();
+		}
+	}
 
-  private Collection<Instrument> instruments;
+	private Collection<Instrument> instruments;
 
-  public void setInstruments(Collection<Instrument> instruments) {
-    this.instruments = instruments; //<co id="co_injectInstrumentCollection"/>
-  }
+	// 注入instruments集合
+	public void setInstruments(Collection<Instrument> instruments) {
+		this.instruments = instruments;
+	}
 }
-//<end id="onemanband_java" />
--- springidol/src/test/java/com/springinaction/springidol/IdolTest.java
+++ springidol/src/test/java/com/springinaction/springidol/IdolTest.java
@@ -1,7 +1,7 @@
 package com.springinaction.springidol;
 
-import static java.lang.System.*;
-import static org.junit.Assert.*;
+import static java.lang.System.setProperty;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,34 +12,34 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration( { "classpath:com/springinaction/springidol/spring-idol.xml" })
+@ContextConfiguration({ "classpath:com/springinaction/springidol/spring-idol.xml" })
 public class IdolTest {
 
-  @BeforeClass
-  public static void setup() {
-    setProperty("STANS_SONG", "Total Eclipse of the Heart");
-  }
+	@BeforeClass
+	public static void setup() {
+		setProperty("STANS_SONG", "Total Eclipse of the Heart");
+	}
 
-  @Autowired
-  ApplicationContext context;
+	@Autowired
+	ApplicationContext context;
 
-  @Test
-  public void carlShouldBeWiredWithKennysSong() {
-    Instrumentalist kenny = (Instrumentalist) context.getBean("kenny");
-    Instrumentalist carl = (Instrumentalist) context.getBean("carl");
-    assertEquals(kenny.getSong(), carl.getSong());
-  }
+	@Test
+	public void carlShouldBeWiredWithKennysSong() {
+		Instrumentalist kenny = (Instrumentalist) context.getBean("kenny");
+		Instrumentalist carl = (Instrumentalist) context.getBean("carl");
+		assertEquals(kenny.getSong(), carl.getSong());
+	}
 
-  @Test
-  public void stanShouldBeWiredWithSongFromSystemProperties() {
-    Instrumentalist stan = (Instrumentalist) context.getBean("stan");
-    assertEquals("Total Eclipse of the Heart", stan.getSong());
-  }
+	@Test
+	public void stanShouldBeWiredWithSongFromSystemProperties() {
+		Instrumentalist stan = (Instrumentalist) context.getBean("stan");
+		assertEquals("Total Eclipse of the Heart", stan.getSong());
+	}
 
-  @Test
-  public void rickyShouldBeWiredWithSongFromSongBook() {
-    Instrumentalist ricky = (Instrumentalist) context.getBean("ricky");
-    assertEquals("That Old Black Magic", ricky.getSong());
-  }
+	@Test
+	public void rickyShouldBeWiredWithSongFromSongBook() {
+		Instrumentalist ricky = (Instrumentalist) context.getBean("ricky");
+		assertEquals("That Old Black Magic", ricky.getSong());
+	}
 
 }
